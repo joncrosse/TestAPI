f899f0665c9bf8c9bb14d8d5b13a3ce2b7da439f
Jon Crosse <jon.crosse@aol.com>
2020-05-01 03:31:30 -0400

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'welcome/index'
-  root 'welcome#index'
 
   get 'api/status'
   post 'api/status'
@@ -10,4 +8,14 @@ Rails.application.routes.draw do
   post 'api/seeuser/:id', to: 'api#seeuser'
   
   post 'api/createuser'
+
+  post 'api/unfollow/:id', to: 'api#unfollow'
+
+  post 'api/follow/:id', to: 'api#follow'
+
+  post 'api/poststory'
+
+  post 'api/block/:id', to: 'api#block'
+
+  post 'api/reprint/:id', to: 'api#reprint'
 end
