a897c3fb304666853235dc213aa9d336d03795c3
Jon Crosse <jon.crosse@aol.com>
2020-05-01 14:59:19 -0400

    Add files via upload

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -108,17 +108,30 @@ class ApiController < ApplicationController
 
     def follow
       authverification = param_auth(params[:handle], params[:password])
+      guSQL = "select idnum from Identity where handle = '#{params[:handle]}'"
+      getUser = ActiveRecord::Base.connection.exec_query(guSQL).as_json;
+
+      checkBlocked = "select handle, 
+                        fullname, 
+                        from Identity i
+                        LEFT OUTER JOIN Block b ON (i.idnum = b.idnum)
+                        where NOT EXISTS (select * from Block where idnum = #{params[:id]} AND blocked = #{getUser[0]["idnum"]}) AND i.idnum = #{params[:id]} GROUP BY i.idnum;"
+
+      isblocked = ActiveRecord::Base.connection.exec_query(checkBlocked)
+
 
       if authverification === '1'
         respond_to do |format|
             msg = {"status_code":"-10", "error":"invalid credentials"}
             format.json  { render :json => msg } # don't do msg.to_json
         end
+      elsif isblocked.blank?
+        respond_to do |format|
+          msg = { :status => "0", :data => "User not found"}
+          format.json  { render :json => msg } # don't do msg.to_json
+        end
+        
       else
-      guSQL = "select idnum from Identity where handle = '#{params[:handle]}'"
-      getUser = ActiveRecord::Base.connection.exec_query(guSQL).as_json;
-
-
       sql = "insert into Follows 
                 (follower, 
                 followed, 
@@ -126,6 +139,7 @@ class ApiController < ApplicationController
              values(#{getUser[0]["idnum"]},#{params[:id]},now());"
 
       addFollow = ActiveRecord::Base.connection.exec_query(sql)
+      
 
       respond_to do |format|
         msg = { :status => "Followed"}
@@ -157,11 +171,20 @@ class ApiController < ApplicationController
                          '#{params[:url]}', 
                          '#{params[:expires]}',
                          now());"
-        
+        begin
         sendStory = ActiveRecord::Base.connection.exec_query(psSQL)
+        rescue
+          respond_to do |format|
+            msg = { :status => "0", :error => "Invalid expires date"}
+            format.json  { render :json => msg } # don't do msg.to_json
+  
+          end
+        else
         respond_to do |format|
-          msg = { :status => "Story Posted"}
+          msg = { :status => "1"}
           format.json  { render :json => msg } # don't do msg.to_json
+
+        end
         end 
       end
     end
@@ -215,5 +238,41 @@ class ApiController < ApplicationController
           format.json  { render :json => msg } # don't do msg.to_json
           end 
         end
+    end
+
+    def suggestions
+      authverification = param_auth(params[:handle], params[:password])
+      guSQL = "select idnum from Identity where handle = '#{params[:handle]}'"
+      getUser = ActiveRecord::Base.connection.exec_query(guSQL).as_json;
+        
+      suggSQL = "select a.idnum, a.handle from Identity a
+                JOIN Follows x ON(a.idnum = x.followed)
+                WHERE x.follower IN (SELECT followed FROM Follows WHERE follower = #{getUser[0]["idnum"]})
+                AND
+                x.followed NOT IN (SELECT followed FROM Follows WHERE follower = #{getUser[0]["idnum"]})
+                AND a.idnum != #{getUser[0]["idnum"]}
+                GROUP BY a.idnum
+                LIMIT 4;"
+
+      getSugg = ActiveRecord::Base.connection.exec_query(suggSQL)
+
+      if authverification === '1'
+        respond_to do |format|
+            msg = {"status_code":"-10", "error":"invalid credentials"}
+            format.json  { render :json => msg } # don't do msg.to_json
+        end
+
+      elsif getSugg.blank?
+        respond_to do |format|
+          msg = { :status => "0", :data => "No suggestions found"}
+          format.json  { render :json => msg } # don't do msg.to_json
+        end
+
+      else
+        respond_to do |format|
+          msg = { :status => "Access Granted", :data => getSugg}
+          format.json  { render :json => msg } # don't do msg.to_json
+        end
+      end
     end
 end
